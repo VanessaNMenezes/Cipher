1c35327ea4028ae5205d01dbb1a68b084882f517
vanessa <vanessa_menezes_18@hotmail.com>
2023-05-20 19:26:14 -0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+const encryptButton = document.getElementById("encrypt");
+encryptButton.addEventListener("click", function () {
+  const originalMessage = document.getElementById("message").value;
+  const offSet = document.getElementById("displacement").value;
+  if (offSet !== "") {
+    const encryptedMessage = cipher.encode(parseInt(offSet), originalMessage);
+    document.getElementById("result").value = encryptedMessage;
+  } else if (originalMessage === "" ) {
+    alert("Por favor, insira uma mensagem para criptografar!");
+  } else {
+    alert("Por favor, escolha um número para a chave secreta!");
+  }
+});
+
+const decryptButton = document.getElementById("decrypt");
+decryptButton.addEventListener("click", function () {
+  const originalMessage = document.getElementById("message").value;
+  const offSet = document.getElementById("displacement").value;
+  if (offSet !== "") {
+    const decryptedMessage = cipher.decode(parseInt(offSet), originalMessage);
+    document.getElementById("result").value = decryptedMessage;
+  } else if (originalMessage === "" ) {
+    alert("Por favor, insira uma mensagem para descriptografar!");
+  } else {
+    alert("Por favor, escolha um número para a chave secreta!");
+  }
+});
+
+const clean = () => {
+    
+  document.getElementById("result").innerHTML = "";
+  document.querySelector("#message").value = "";
+  document.querySelector("#displacement").value = "";
+  
+}
+document.getElementById("clean").addEventListener("click", clean);
